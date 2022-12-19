f3c44115ad6319d0806bec249f0ff14fd98f8890
Joao Lino <linusmannuel@gmail.com>
2022-12-19 23:10:53 +0100

    feat: add site logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import logo from './images/logo.svg';
 
 function App() {
   return (
-    <div>
-      <header>
-
+    <div className="app">
+      <header className="header">
+        <div className="logo">
+          <img src={logo}></img>
+        </div>
       </header>
     </div>
   );
