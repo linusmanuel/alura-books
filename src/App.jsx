cd4897e2b2b628224f805c855c0d0b8bbd1cd562
Joao Lino <linusmannuel@gmail.com>
2022-12-19 23:19:36 +0100

    feat: menu orizontal align

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import logo from './images/logo.svg';
 function App() {
   return (
     <div className="app">
-      <header className="header">
+      <header className="app-header">
         <div className="logo">
           <img src={logo}></img>
+          <p><strong>Alura</strong>Books</p>
         </div>
       </header>
     </div>
