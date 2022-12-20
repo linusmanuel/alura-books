f2dea566445b3018562063b6768aa9000c3bcbd0
Joao Lino <linusmannuel@gmail.com>
2022-12-21 00:13:43 +0100

    refator: create logo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import './App.css';
-import logo from './images/logo.svg';
-
+import Logo from './componentes/logo'
 function App() {
   return (
     <div className="app">
       <header className="app-header">
-        <div className="logo">
-          <img src={logo}></img>
-          <p><strong>Alura</strong>Books</p>
-        </div>
+        <Logo></Logo>
       </header>
     </div>
   );
