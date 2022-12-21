a9721cecda2140da2470626ee5ebf95bdb61d505
Joao Lino <linusmannuel@gmail.com>
2022-12-21 22:28:11 +0100

    feat: add icones on menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import './App.css';
 import Logo from './componentes/logo'
+import Perfil from './images/perfil.svg'
+import Sacola from './images/sacola.svg'
+
+const textOpcoes = ['Categorias', 'Minha Estande', 'Favoritos']
+const icones = [Perfil, Sacola]
 
 function App() {
   return (
     <div className="app">
       <header className="app-header">
         <Logo />
+        <ul className="opcoes">
+         {
+          textOpcoes.map( (texto) => (
+            <li className='opcao'>{texto}</li>
+          ))
+         }
+        </ul>
+        <ul className='icones'>
+          {
+            icones.map( (icone) => (
+              <li className='icone'><img src={icone} alt="" /></li>
+            ))
+          }
+        </ul>
+
       </header>
     </div>
   );
