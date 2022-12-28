a330dcc74a80a9237276049d06d459750c4c6ad8
Joao Lino <linusmannuel@gmail.com>
2022-12-28 12:18:00 +0100

    feat: Add Input component from styled components

---
+++ src/componentes/Input/index.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+const Input = styled.input`
+  border: 1px solid #fff;
+  background-color: transparent;
+  padding: 20px 140px;
+  border-radius: 50px;
+  width: 200px;
+  color: #fff;
+  font-size: 16px;  
+  margin-bottom: 10px;
+
+  &::placeholder {
+    color: currentColor;
+    font-size: 16px;
+  }
+`
+export default Input
--- src/componentes/Pesquisa/index.jsx
+++ src/componentes/Pesquisa/index.jsx
@@ -1,10 +1,11 @@
+import Input from "../Input"
 
 function Pesquisa() {
   return (
     <section>
       <h1>Já sabe por onde começar?</h1>
       <h2>Encontre seu livro em nossa estande</h2>
-      <input 
+      <Input 
         type="search"
         placeholder="Escreva sua próxima leitura"
       />
